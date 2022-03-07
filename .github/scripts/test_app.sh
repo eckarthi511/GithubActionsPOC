759505ef89705d031cde81c56397273f19c098a2
eckarthi511 <82244223+eckarthi511@users.noreply.github.com>
2022-03-07 14:59:46 -0500

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Github.xcworkspace \
+xcodebuild -workspace Github.xcodeproj \
             -scheme Github\ iOS \
             -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 13 \
             clean test | xcpretty
