5232b543d3f199453dac93747d47b77fbfa90801
Ramachandran, Karthick <Karthick.Ramachandran@CVSHealth.com>
2022-03-16 23:34:52 -0400

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -eo pipefail
+
             
-xcodebuild -project Github.xcodeproj/ -scheme Github -derivedDataPath Build/ -enableCodeCoverage YES clean build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -project Github.xcodeproj \
+            -scheme Github \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
+            -derivedDataPath Build \
+             -enableCodeCoverage YES clean build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 
 clean test
